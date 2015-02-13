c1bba2a0438dc61e543b3d38d73c096b06cf3ec1
Tony Brown <tony@tonybrown.me>
2015-02-13 14:19:41 -0500

    playing around with react

--- excercises/3-events-and-state/app.js
+++ excercises/3-events-and-state/app.js
@@ -2,79 +2,92 @@
 // Excercise:
 // - make these tabs work when you click them
 ////////////////////////////////////////////////////////////////////////////////
+
 var React = require('react');
-var assign = require('react/lib/Object.assign');
-
-var DATA = [
-  { name: 'USA', description: 'Land of the Free, Home of the brave' },
-  { name: 'China', description: 'Lots of concrete' },
-  { name: 'Russia', description: 'World Cup 2018!' },
-];
-
 var App = React.createClass({
-
-  getInitialState () {
-    return {
-      activeTabIndex: 0
-    };
-  },
-
-  renderTabs () {
-    var activeTabIndex = this.state.activeTabIndex;
-    return this.props.countries.map((country, index) => {
-      return (
-        <div
-          onClick = {() => { this.setState({ activeTabIndex: index }) }}
-          style={index === activeTabIndex ? styles.activeTab : styles.tab}>
-          {country.name}
-        </div>
-      );
-    });
-  },
-
-  renderPanel () {
-    var activeTabIndex = this.state.activeTabIndex;
-    var country = this.props.countries[activeTabIndex];
-    return (
-      <div>
-        <p>{country.description}</p>
-      </div>
-    );
-  },
-
   render () {
     return (
-      <div style={styles.app}>
-        <div style={styles.tabs}>
-          {this.renderTabs()}
-        </div>
-        <div style={styles.tabPanels}>
-          {this.renderPanel()}
-        </div>
+      <di>
+        don't want more words I can't spell like exercise.
       </div>
     );
-
   }
 });
 
-var styles = {};
 
-styles.tab = {
-  display: 'inline-block',
-  padding: 10,
-  margin: 10,
-  borderBottom: '4px solid',
-  borderBottomColor: '#ccc',
-  cursor: 'pointer'
-};
-
-styles.activeTab = assign({}, styles.tab, {
-  borderBottomColor: '#000'
-});
-
-styles.tabPanels = {
-  padding: 10
-};
-
-React.render(<App countries={DATA}/>, document.body);
+//var React = require('react');
+//var assign = require('react/lib/Object.assign');
+//
+//var DATA = [
+//  { name: 'USA', description: 'Land of the Free, Home of the brave' },
+//  { name: 'China', description: 'Lots of concrete' },
+//  { name: 'Russia', description: 'World Cup 2018!' },
+//];
+//
+//var App = React.createClass({
+//
+//  getInitialState () {
+//    return {
+//      activeTabIndex: 0
+//    };
+//  },
+//
+//  renderTabs () {
+//    var activeTabIndex = this.state.activeTabIndex;
+//    return this.props.countries.map((country, index) => {
+//      return (
+//        <div
+//          onClick = {() => { this.setState({ activeTabIndex: index }) }}
+//          style={index === activeTabIndex ? styles.activeTab : styles.tab}>
+//          {country.name}
+//        </div>
+//      );
+//    });
+//  },
+//
+//  renderPanel () {
+//    var activeTabIndex = this.state.activeTabIndex;
+//    var country = this.props.countries[activeTabIndex];
+//    return (
+//      <div>
+//        <p>{country.description}</p>
+//      </div>
+//    );
+//  },
+//
+//  render () {
+//    return (
+//      <div style={styles.app}>
+//        <div style={styles.tabs}>
+//          {this.renderTabs()}
+//        </div>
+//        <div style={styles.tabPanels}>
+//          {this.renderPanel()}
+//        </div>
+//      </div>
+//    );
+//
+//  }
+//});
+//
+//var styles = {};
+//
+//styles.tab = {
+//  display: 'inline-block',
+//  padding: 10,
+//  margin: 10,
+//  borderBottom: '4px solid',
+//  borderBottomColor: '#ccc',
+//  cursor: 'pointer'
+//};
+//
+//styles.activeTab = assign({}, styles.tab, {
+//  borderBottomColor: '#000'
+//});
+//
+//styles.tabPanels = {
+//  padding: 10
+//};
+//
+//React.render(<App countries={DATA}/>, document.body);
 

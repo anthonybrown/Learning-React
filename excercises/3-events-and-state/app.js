253c6c920fb43c82431f55d2ea7da333167edb10
Tony Brown <tony@tonybrown.me>
2015-02-13 15:15:09 -0500

    playing with events

--- excercises/3-events-and-state/app.js
+++ excercises/3-events-and-state/app.js
@@ -3,78 +3,132 @@
 // - make these tabs work when you click them
 ////////////////////////////////////////////////////////////////////////////////
 var React = require('react');
-var assign = require('react/lib/Object.assign');
 
-var DATA = [
-  { name: 'USA', description: 'Land of the Free, Home of the brave' },
-  { name: 'China', description: 'Lots of concrete' },
-  { name: 'Russia', description: 'World Cup 2018!' },
-];
+var alertStuff = (msg) => {
+  alert(msg)
+};
+
+/*
+ *
+ * here we are add alert stuf
+ * as a method to our App class
+ */
 
 var App = React.createClass({
-
-  getInitialState () {
-    return {
-      activeTabIndex: 0
-    };
+  alertStuff (msg) {
+    alert(msg);
   },
 
-  renderTabs () {
-    var activeTabIndex = this.state.activeTabIndex;
-    return this.props.countries.map((country, index) => {
+    render () {
       return (
-        <div
-          onClick = {() => { this.setState({ activeTabIndex: index }) }}
-          style={index === activeTabIndex ? styles.activeTab : styles.tab}>
-          {country.name}
+        <div>
+          <button onClick={this.alertStuff.bind(this, 'hello')}>Hello</button>
+          <button onClick={this.alertStuff.bind(this, 'goodbye')}>Bye</button>
         </div>
       );
-    });
-  },
-
-  renderPanel () {
-    var activeTabIndex = this.state.activeTabIndex;
-    var country = this.props.countries[activeTabIndex];
-    return (
-      <div>
-        <p>{country.description}</p>
-      </div>
-    );
-  },
-
-  render () {
-    return (
-      <div style={styles.app}>
-        <div style={styles.tabs}>
-          {this.renderTabs()}
-        </div>
-        <div style={styles.tabPanels}>
-          {this.renderPanel()}
-        </div>
-      </div>
-    );
-
-  }
+    }
 });
 
-var styles = {};
 
-styles.tab = {
-  display: 'inline-block',
-  padding: 10,
-  margin: 10,
-  borderBottom: '4px solid',
-  borderBottomColor: '#ccc',
-  cursor: 'pointer'
-};
+/*
+ * Make a button with React.js
+ * and bind it to an event handler
+ *
+ * */
 
-styles.activeTab = assign({}, styles.tab, {
-  borderBottomColor: '#000'
-});
+//var App = React.createClass({
+//  render() {
+//    return (
+//      <div>
+//        <button onClick = {alertStuff.bind(this, 'hello')}>Hello</button>
+//        <button onClick = {alertStuff.bind(this, 'goodbye')}>goodbye</button>
+//      </div>
+//    );
+//  }
+//});
 
-styles.tabPanels = {
-  padding: 10
-};
+React.render(<App />, document.body);
+//
+// make a button and append it to the DOM
+// with vanilla JavaScript
+//var btn = document.createElement('button');
+//btn.onclick = () => alert('hello, easy peasy wasn\'t it?');
+//document.body.appendChild(btn);
 
-React.render(<App countries={DATA}/>, document.body);
+
+//var React = require('react');
+//var assign = require('react/lib/Object.assign');
+//
+//var DATA = [
+//  { name: 'USA', description: 'Land of the Free, Home of the brave' },
+//  { name: 'China', description: 'Lots of concrete' },
+//  { name: 'Russia', description: 'World Cup 2018!' },
+//];
+//
+//var App = React.createClass({
+//
+//  getInitialState () {
+//    return {
+//      activeTabIndex: 0
+//    };
+//  },
+//
+//  renderTabs () {
+//    var activeTabIndex = this.state.activeTabIndex;
+//    return this.props.countries.map((country, index) => {
+//      return (
+//        <div
+//          onClick = {() => { this.setState({ activeTabIndex: index }) }}
+//          style={index === activeTabIndex ? styles.activeTab : styles.tab}>
+//          {country.name}
+//        </div>
+//      );
+//    });
+//  },
+//
+//  renderPanel () {
+//    var activeTabIndex = this.state.activeTabIndex;
+//    var country = this.props.countries[activeTabIndex];
+//    return (
+//      <div>
+//        <p>{country.description}</p>
+//      </div>
+//    );
+//  },
+//
+//  render () {
+//    return (
+//      <div style={styles.app}>
+//        <div style={styles.tabs}>
+//          {this.renderTabs()}
+//        </div>
+//        <div style={styles.tabPanels}>
+//          {this.renderPanel()}
+//        </div>
+//      </div>
+//    );
+//
+//  }
+//});
+//
+//var styles = {};
+//
+//styles.tab = {
+//  display: 'inline-block',
+//  padding: 10,
+//  margin: 10,
+//  borderBottom: '4px solid',
+//  borderBottomColor: '#ccc',
+//  cursor: 'pointer'
+//};
+//
+//styles.activeTab = assign({}, styles.tab, {
+//  borderBottomColor: '#000'
+//});
+//
+//styles.tabPanels = {
+//  padding: 10
+//};
+//
+//React.render(<App countries={DATA}/>, document.body);
 
